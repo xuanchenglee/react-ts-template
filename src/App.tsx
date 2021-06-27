5e2274efa532ed0e5a4f376d70417eac2691b860
JQ <sjjuge2@gmail.com>
2021-06-27 12:24:46 +1000

    render svg and png image and import styling sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
-export const App = ()=>{
-    return <h1>React TS Webpack Starter Template</h1> 
-}
+import "./styles/styles.css";
+import IMAGE from "./assets/react.png";
+import LOGO from "./assets/react.svg";
+
+export const App = () => {
+  return (
+    <>
+      <h1>React TS Webpack Starter Template</h1>;
+      <img src={IMAGE} alt="React Logo" width="300" height="300" />
+      <img src={LOGO} alt="React Logo" width="300" />
+    </>
+  );
+};
