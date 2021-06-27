9cb4697d48da0df5344b2165ad5a897f381efefe
JQ <sjjuge2@gmail.com>
2021-06-27 13:10:51 +1000

    config wepack for multiple environments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import LOGO from "./assets/react.svg";
 export const App = () => {
   return (
     <>
-      <h1>React TS Webpack Starter Template</h1>;
+      <h1>
+        React TS Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
+      ;
       <img src={IMAGE} alt="React Logo" width="300" height="300" />
       <img src={LOGO} alt="React Logo" width="300" />
     </>
