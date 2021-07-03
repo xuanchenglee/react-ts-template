a526a4444c774c60029d36b449edad19a4222258
JQ <sjjuge2@gmail.com>
2021-07-03 12:59:09 +1000

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <>
       <h1>
-        React TS Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        React TS Webpack Starter Template - {process.env.NODE_ENV} -
         {process.env.name}
       </h1>
       <img src={IMAGE} alt="React Logo" width="300" height="300" />
